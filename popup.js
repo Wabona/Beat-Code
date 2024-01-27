a70f6f22a08fd4918514fe353d1464f46efdb612
kristel7k <kmkouatchou7@gmail.com>
2024-01-26 20:07:39 -0500

    add js file and buttons

---
+++ popup.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var generateButton = document.getElementById('generateQuiz');
+    var pastQuestionsButton = document.getElementById('pastQuestions');
+    
+    // button click event handlers
+
+    generateButton.addEventListener('click', function () {
+      // code to generate quiz
+      console.log('generating quiz questions...');
+    });
+  
+    pastQuestionsButton.addEventListener('click', function () {
+      // code to find past quizzez
+      console.log('Accessing past quiz questions...');
+    });
+  });
