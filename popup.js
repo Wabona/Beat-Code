69d72a3ca43e54d21014ff3e83f186cf1ee2b651
Wabona D <wdandi619@gmail.com>
2024-01-27 16:13:54 -0500

    backend updates

--- popup.js
+++ popup.js
@@ -1,58 +1,116 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var generateButton = document.getElementById('generateQuiz');
-  var pastQuestionsButton = document.getElementById('pastQuestions');
+document.addEventListener("DOMContentLoaded", function () {
+  var generateButton = document.getElementById("generateQuiz");
+  var pastQuestionsButton = document.getElementById("pastQuestions");
 
   // letting background script know popup opened
   chrome.runtime.sendMessage({ action: "popupOpened" });
 
-
-  // button click event handlers
-
-    function quizScreen(){
-      var quizContainer = document.getElementById('quiz-container');
-      quizContainer.innerHTML = '';
-
-      var quizHtml = ``;
+  // listening for response from server
+  chrome.runtime.onMessage.addListener(async function (
+    message,
+    sender,
+    sendResponse
+  ) {
+    if (message.action === "serverResponse") {
+      console.log("Response from server:", message.data);
     }
-    
-    // checking the answer for the quiz
-    function checkAnswer(){
+  });
 
-  }
-
-  generateButton.addEventListener('click', function () {
+  // handling generate button action
+  generateButton.addEventListener("click", function () {
+    quizScreen();
     // send message to background.js script to take screenshot of webpage and generate questions
-    chrome.runtime.sendMessage({action: "takeScreenshot"}, function(response){
-      console.log("Received response: ", response);
-    });
-
-    chrome.runtime.onMessage.addListener(async function(message, sender, sendResponse) {
-      if (message.action === "serverResponse") {
-          console.log("Response from server:", message.data);
+    chrome.runtime.sendMessage(
+      { action: "takeScreenshot" },
+      function (response) {
+        console.log("Received response: ", response);
       }
-    });
+    );
 
-    // switch screens
-    //quizScreen();
-
-    console.log('generating quiz questions...');
-
+    console.log("generating quiz questions...");
   });
 
-    
-    // generateButton.addEventListener('click', function () {
-    //   // code to generate quiz
-    //   //fetch("http://backend/question");
-    //   console.log('generating quiz questions...');
-    // });
-  
-    function pastQuizScreen(){
-      window.location.href = "pastquiz.html";
-    }
-    pastQuestionsButton.addEventListener('click', function () {
-      // code to find past quizzez
-      //fetch("http://backend/question");
-      pastQuizScreen();
-      console.log('Accessing past quiz questions...');
-    });
+  function pastQuizScreen() {
+    window.location.href = "pastquiz.html";
+  }
+  pastQuestionsButton.addEventListener("click", function () {
+    // code to find past quizzez
+    //fetch("http://backend/question");
+    pastQuizScreen();
+    console.log("Accessing past quiz questions...");
   });
+});
+
+function quizScreen() {
+  var quizContainer = document.getElementById('quiz-container');
+  quizContainer.innerHTML = '';
+
+  var quizHtml = `
+      <h1>BeatCode LeetCode Quiz</h1>
+      <div class="question">
+        <h1>What type of traversal did you use to search the binary tree?</h1>
+      </div>
+      <div class="options">
+        <span>
+            <input
+            type="radio"
+            id="option-one"
+            name="option"
+            class="radio"
+            value="optionA"
+            />
+            <label for="option-one" class="option" id="option-one-label"
+            >Inorder Traversal</label
+            >
+        </span>
+
+        <span>
+            <input
+            type="radio"
+            id="option-two"
+            name="option"
+            class="radio"
+            value="optionB"
+            />
+            <label for="option-two" class="option" id="option-two-label"
+            >Preorder Traversal</label
+            >
+        </span>
+
+        <span>
+            <input
+            type="radio"
+            id="option-three"
+            name="option"
+            class="radio"
+            value="optionC"
+            />
+            <label for="option-three" class="option" id="option-three-label"
+            >Postorder Traversal</label
+            >
+        </span>
+
+        <span>
+            <input
+            type="radio"
+            id="option-four"
+            name="option"
+            class="radio"
+            value="optionD"
+            />
+            <label for="option-four" class="option" id="option-four-label"
+            >Boundary Traversal</label
+            >
+        </span>
+
+      </div>
+      <div class="buttons">
+        <button>Submit</button>
+        <button>Next Question></button>
+      </div>
+
+      <div id="result"></div>
+    `;
+
+  quizContainer.innerHTML = quizHtml;
+}
