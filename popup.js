7a318d3486024d9924502dc919207e87b0841643
kristel7k <kmkouatchou7@gmail.com>
2024-01-26 21:21:03 -0500

    implement quiz screen

--- popup.js
+++ popup.js
@@ -4,6 +4,15 @@ document.addEventListener('DOMContentLoaded', function () {
     
     // button click event handlers
 
+    function quizScreen(){
+        window.location.href = 'quiz.html';
+    }
+
+    generateButton.addEventListener('click', function () {
+        // switch screens
+        quizScreen();
+    });
+
     generateButton.addEventListener('click', function () {
       // code to generate quiz
       fetch("http://backend/question");
@@ -12,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
   
     pastQuestionsButton.addEventListener('click', function () {
       // code to find past quizzez
+      fetch("http://backend/question");
       console.log('Accessing past quiz questions...');
     });
   });
